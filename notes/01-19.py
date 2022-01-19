05bb11a740273ce7a20ed6aab338a337105229b8
Glenn Downing <downing@cs.utexas.edu>
2022-01-18 21:37:41 -0600

    another commit

--- notes/01-19.py
+++ notes/01-19.py
@@ -73,3 +73,63 @@ Specifications Grading
 Website
 	https://www.cs.utexas.edu/users/downing/cs373/Schedule.html
 """
+
+"""
+Syllabus
+
+
+
+
+
+Getting to know someone
+1. What's the story of your name?
+2. What app do you use the most on your phone?
+3. What failure set you up for later success?
+4. What contrary opinion to you hold?
+5. Exchange contact info.
+
+
+
+
+
+UT Programming Contest (UTPC)
+Association of Computing Machinery (ACM) is 80 years old
+International Collegiate Programming Contest (ICPC) is 40 years old
+annual collegiate competitive programming contest
+teams of 3, using C, C++, Java, or Python
+biweekly contests at UT
+around 110 regions around the world
+our regional contest: South Central USA
+around 65 teams, 25 schools, 3 states
+we send 4 teams
+2014: 2, M
+2015, 2, T
+2016: 1, So
+2017: 1, Be
+2018: 1, Po
+2019: 1, Mo
+2020: -, Ba
+
+https://www.cs.utexas.edu/users/utpc/
+https://discord.gg/hjQAveZ
+
+CS 104c: Competitive Programming
+https://www.cs.utexas.edu/users/downing/cs104c/
+weekly, Fridays, 2 pm, GDC 1.304
+
+
+
+
+
+UT Chess
+https://www.sites.utexas.edu/utchessclub/
+https://discord.gg/f2eMxzW
+weekly, Mondays, 6 pm, Dobie Food Court
+
+
+
+
+
+Google "cs371p fall 2021 final entry"
+Perusall Syllabus assignment
+"""
