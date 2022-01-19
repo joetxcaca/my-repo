189a40b7c4c4cc604f2208d371be71ce9bb70837
Glenn Downing <downing@cs.utexas.edu>
2022-01-18 18:10:53 -0600

    another commit

---
+++ java/Hello.java
@@ -0,0 +1,29 @@
+// ----------
+// Hello.java
+// ----------
+
+final class Hello {
+    public static void main (String[] args) {
+        System.out.println("Nothing to be done.");}}
+
+/*
+Developed in 1995 by James Gosling of Canada, now at AWS.
+Javas is procedural, object-oriented, statically typed, and garbage
+collected.
+
+
+
+% javac --version
+javac 13.0.2
+
+
+
+% java --version
+java 13.0.2 2020-01-14
+
+
+
+% javac -Xlint Hello.java
+% java  -ea    Hello
+Nothing to be done.
+*/
