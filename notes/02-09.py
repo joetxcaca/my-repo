834e0bb2401b39ac04705446f05c561d8e4fae3b
Glenn Downing <downing@cs.utexas.edu>
2022-02-09 18:44:41 -0600

    another commit

--- notes/02-09.py
+++ notes/02-09.py
@@ -90,10 +90,114 @@ huge disconnect between lectures and Web project
 """
 
 """
-Paper #03: 2022-02-06: Continuous Integration
+Paper #4: 2022-02-13: Pair Programming
 you MUST go to Perusall THROUGH Canvas
 """
 
 """
 Quiz #7
 """
+
+"""
+first competitive programming contest
+Fri, 11 Feb, 5:30 - 8 pm CT
+Zoom or GDC auditorium
+free food!
+"""
+
+#!/usr/bin/env python3
+
+# pylint: disable = eval-used
+# pylint: disable = invalid-name
+# pylint: disable = line-too-long
+# pylint: disable = literal-comparison
+# pylint: disable = missing-docstring
+
+# -------------
+# Exceptions.py
+# -------------
+
+# https://docs.python.org/3/library/exceptions.html
+
+def f (b: bool) -> int :
+    if b :
+        raise NameError("abc") # creating a NameError object, one-arg constructor
+    return 0
+
+def test1 () -> None :
+    try :
+        assert f(False) == 0
+    except NameError :       # pragma: no cover
+        assert False
+    else :                   # no exception vs. raised and handled, but not with break, continue, return
+        pass
+    finally :                # always, even with break, continue, return
+        pass
+
+def test2 () -> None :
+    try :
+        assert f(True) == 1
+        assert False                         # pragma: no cover
+    except NameError as e : # e is a reference (address) to the NameError object
+        assert isinstance(e,      NameError)
+        assert isinstance(e.args, tuple)
+        assert len(e.args)  ==     1
+        assert e.args       is not ("abc",)
+        assert e.args       ==     ("abc",)
+    else :                                   # no exception vs. raised and handled, but not with break, continue, return
+        assert False                         # pragma: no cover
+    finally :                                # always, even with break, continue, return
+        pass
+
+def test3 () -> None :
+    assert issubclass(NameError,     Exception)
+    assert issubclass(Exception,     BaseException)
+    assert issubclass(BaseException, object)
+
+def main () -> None :
+    print("Exceptions.py")
+    for i in range(3) :
+        eval("test" + str(i + 1) + "()")
+    print("Done.")
+
+if __name__ == "__main__" : # pragma: no cover
+    main()
+
+a = [2, 3, 4]
+b = [2, 3, 4]
+print(a == b) # value    comparison, true, NOT like Java's ==, it is like Java's .equal()
+print(a is b) # identity comparison, false
+
+a = [2, 3, 4]
+b = a
+print(a == b) # true
+print(a is b) # true
+
+# this is Java
+
+class A {}
+
+x = new A();
+y = new A();
+System.out.println(x.equals(y)) # identity comparison, false
+
+a = [2, 3, 4]
+print(type(a)) # list
+
+a = [2, 3]
+print(type(a)) # list
+
+a = [2]
+print(type(a)) # list
+
+a = (2, 3, 4)
+print(type(a)) # tuple, immutable list, BOTH in content and structure
+
+a = (2, 3)
+print(type(a)) # tuple
+
+a = (2)
+print(type(a)) # int!!!
+
+a = (2,)
+print(type(a)) # tuple
