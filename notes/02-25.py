0f740eca9963ef187b40427d219b44e22cfb29ba
Glenn Downing <downing@cs.utexas.edu>
2022-02-25 11:51:26 -0600

    another commit

--- notes/02-25.py
+++ notes/02-25.py
@@ -124,3 +124,313 @@ Project #2: IDB1
 https://www.cs.utexas.edu/users/downing/cs373/Groups.html
 """
 
+"""
+programming contest, teams
+"""
+
+# Types.py
+
+def test15 () :
+    class A :
+        def bar (foo, i, s, f) :
+            foo.i = i
+            foo.s = s
+            foo.f = f
+
+        def __eq__ (self, rhs) :
+            return (self.i == rhs.i) and (self.s == rhs.s) and (self.f == rhs.f)
+
+	x = A()
+	x.bar(2, "abc", 3.45)
+	delete x.i
+	y = A()
+#    x = A(2, "abc", 3.45)
+#    y = A(2, "abc", 3.45)
+    assert x is not y
+    assert x ==     y # x.__eq__(y), also like A.__eq__(x, y)
+
+# self in Python is like this in Java
+
+reduce(operator.add, [2, 3, 4], 0) -> 9
+
+# reduce is an example of a higher-order function, that is a function that either takes or returns another function
+
+reduce(A.__eq__, ...)
+
+"""
+Python exhibits an open   object model
+Java   exhibits a  closed object model
+"""
+
+# Java
+
+class A {
+	int    i;
+	string s;
+	float  f;}
+
+# Factorial.py
+
+    def test6 (self) :
+        for f in self.a :
+            with self.subTest(msg=f.__name__) :
+                print()
+                print(f.__name__)
+                if f.__name__ == "factorial" :
+                    t = timeit.timeit(
+                        "math." + f.__name__ + "(100)", # "math.factorial(100)"
+                        "import math",
+                        number = 1000)
+                else :
+                    t = timeit.timeit(
+                        "__main__." + f.__name__ + "(100)", # "__main__.factorial_recursion(100)"
+                        "import __main__",
+                        number = 1000)
+                print(f"{t*1000:.2f} milliseconds")
+
+# 1.23 milliseconds
+
+T = typing.TypeVar("T")
+
+def f (a, b) : # a function that is callable
+	...
+
+lambda (a, b) : ... # a lambda that is callable
+
+class A :
+	def __len__ (...) # len(x)
+
+	def __eq__ (...)  # x == y
+
+	def __call__ (...) # x(), an instance of A is now callable!!!
+
+def reduce_for (bf: typing.Callable[[T, T], T], a: typing.Iterable[T], v: T) -> T :
+    for w in a :
+        v = bf(v, w)
+    return v
+
+def reduce_while (bf: typing.Callable[[T, T], T], a: typing.Iterable[T], v: T) -> T :
+    p = iter(a)
+    try :
+        while True :
+            w = next(p)
+            v = bf(v, w)
+    except StopIteration :
+        pass
+    return v
+
+#!/usr/bin/env python3
+
+# pylint: disable = chained-comparison
+# pylint: disable = eval-used
+# pylint: disable = invalid-name
+# pylint: disable = literal-comparison
+# pylint: disable = missing-docstring
+# pylint: disable = too-few-public-methods
+
+# ------------
+# Operators.py
+# ------------
+
+# https://docs.python.org/3/library/operator.html
+# https://graphics.stanford.edu/~seander/bithacks.html
+
+from copy     import copy, deepcopy
+from operator import add
+
+def test1 () :
+    i = 2
+    j = -i         # negation
+    assert i ==  2
+    assert j == -2
+
+def test2 () :
+    i = 2
+    j = 3
+    k = i + j     # addition
+    assert i == 2
+    assert j == 3
+    assert k == 5
+
+def test3 () :
+    i = 2
+    j = 3
+    k = operator.add(i, j) # addition
+    assert i == 2
+    assert j == 3
+    assert k == 5
+
+reduce(+,            ...) # no
+reduce(operator.add, ...) # yes
+
+def test4 () :
+    i = 2
+    j = 3
+    i += j        # in-place addition
+    assert i == 5
+    assert j == 3
+
+def test5 () :
+    i = 4
+    j = 2
+    f = i / j                   # true division
+    assert i == 4
+    assert j == 2
+    assert isinstance(f, float)
+    assert f == 2.0
+
+def test6 () :
+    i = 5
+    j = 2
+    k = i // j                # floor division
+    assert i == 5
+    assert j == 2
+    assert isinstance(k, int)
+    assert k == 2
+
+def test7 () :
+    f = 5.0
+    j = 2
+    g = f // j                  # floor division
+    assert f == 5.0
+    assert j == 2
+    assert isinstance(g, float)
+    assert g == 2.0
+
+def test8 () :
+    i = 5
+    j = 2
+    k = i % j     # mod
+    assert i == 5
+    assert j == 2
+    assert k == 1
+
+def test9 () :
+    i = 5
+    j = 2
+    i %= j        # in-place mod
+    assert i == 1
+    assert j == 2
+
+def test10 () :
+    i = 2
+    j = 3
+    k = i ** j    # exponentiation
+    assert i == 2
+    assert j == 3
+    assert k == 8
+
+def test11 () :
+    i = 2
+    j = 3
+    i **= j       # in-place exponentiation
+    assert i == 8
+    assert j == 3
+
+def test12 () :
+    i = 2
+    j = ~i         # bit complement
+    k = ~j
+    assert i ==  2
+    assert j == -3
+    assert k ==  2
+
+def test13 () :
+    i = 2
+    j = 3
+    k = i << j     # bit shift left
+    assert i ==  2
+    assert j ==  3
+    assert k == 16
+
+def test14 () :
+    i = 2
+    j = 3
+    i <<= j        # in-place bit shift left
+    assert i == 16
+    assert j ==  3
+
+def test15 () :
+    i = 10         # 1010
+    j = 12         # 1100
+    k = i & j      # 1000: bit and
+    assert i == 10
+    assert j == 12
+    assert k ==  8
+
+def test16 () :
+    i = 10
+    j = 12
+    i &= j         # in-place bit and
+    assert i ==  8
+    assert j == 12
+
+def test17 () :
+    i = 10         # 1010
+    j = 12         # 1100
+    k = i | j      # 1110: bit or
+    assert i == 10
+    assert j == 12
+    assert k == 14
+
+def test18 () :
+    i = 10
+    j = 12
+    i |= j         # in-place bit or
+    assert i == 14
+    assert j == 12
+
+def test19 () :
+    i = 10         # 1010
+    j = 12         # 1100
+    k = i ^ j      # 0110: bit exclusive or
+    assert i == 10
+    assert j == 12
+    assert k ==  6
+
+def test20 () :
+    i = 10
+    j = 12
+    i ^= j         # in-place bit exclusive or
+    assert i ==  6
+    assert j == 12
+
+def test21 () : # only ints
+    i = 10
+    j = 12
+    i ^= j
+    j ^= i
+    i ^= j
+    assert i == 12
+    assert j == 10
+
+	i, j = j, i # for anything
+
+def test22 () :
+    a = True
+    b = True
+    c = False
+    assert a and b
+    assert not (a and c)
+    assert a or b
+    assert a or c
+
+def test23 () :
+    s = "abc"
+    assert s[1]             == "b" # str index
+    assert s.__getitem__(1) == "b"
+    assert str.__getitem__(s, 1) == "b"
+    #s[1] = "d"                    # TypeError: 'str' object does not support item assignment
+
+def test24 () :
+    a = [2, 3, 4]
+    assert a[1]             == 3 # list index
+    assert a.__getitem__(1) == 3 # list.__getitem__(a, 1)
+    a[1] = 5
+    assert a == [2, 5, 4]
+
+def test25 () :
+    u = (2, 3, 4)
+    assert u[1]             == 3 # tuple index
+    assert u.__getitem__(1) == 3 # tuple.__getitem__(u, i)
+    #u[1] = 5                    # TypeError: 'tuple' object does not support item assignment
---
+++ python/Operators.py
@@ -0,0 +1,433 @@
+#!/usr/bin/env python3
+
+# pylint: disable = chained-comparison
+# pylint: disable = eval-used
+# pylint: disable = invalid-name
+# pylint: disable = literal-comparison
+# pylint: disable = missing-docstring
+# pylint: disable = too-few-public-methods
+
+# ------------
+# Operators.py
+# ------------
+
+# https://docs.python.org/3/library/operator.html
+# https://graphics.stanford.edu/~seander/bithacks.html
+
+from copy     import copy, deepcopy
+from operator import add
+
+def test1 () :
+    i = 2
+    j = -i         # negation
+    assert i ==  2
+    assert j == -2
+
+def test2 () :
+    i = 2
+    j = 3
+    k = i + j     # addition
+    assert i == 2
+    assert j == 3
+    assert k == 5
+
+def test3 () :
+    i = 2
+    j = 3
+    k = add(i, j) # addition
+    assert i == 2
+    assert j == 3
+    assert k == 5
+
+def test4 () :
+    i = 2
+    j = 3
+    i += j        # in-place addition
+    assert i == 5
+    assert j == 3
+
+def test5 () :
+    i = 4
+    j = 2
+    f = i / j                   # true division
+    assert i == 4
+    assert j == 2
+    assert isinstance(f, float)
+    assert f == 2.0
+
+def test6 () :
+    i = 5
+    j = 2
+    k = i // j                # floor division
+    assert i == 5
+    assert j == 2
+    assert isinstance(k, int)
+    assert k == 2
+
+def test7 () :
+    f = 5.0
+    j = 2
+    g = f // j                  # floor division
+    assert f == 5.0
+    assert j == 2
+    assert isinstance(g, float)
+    assert g == 2.0
+
+def test8 () :
+    i = 5
+    j = 2
+    k = i % j     # mod
+    assert i == 5
+    assert j == 2
+    assert k == 1
+
+def test9 () :
+    i = 5
+    j = 2
+    i %= j        # in-place mod
+    assert i == 1
+    assert j == 2
+
+def test10 () :
+    i = 2
+    j = 3
+    k = i ** j    # exponentiation
+    assert i == 2
+    assert j == 3
+    assert k == 8
+
+def test11 () :
+    i = 2
+    j = 3
+    i **= j       # in-place exponentiation
+    assert i == 8
+    assert j == 3
+
+def test12 () :
+    i = 2
+    j = ~i         # bit complement
+    k = ~j
+    assert i ==  2
+    assert j == -3
+    assert k ==  2
+
+def test13 () :
+    i = 2
+    j = 3
+    k = i << j     # bit shift left
+    assert i ==  2
+    assert j ==  3
+    assert k == 16
+
+def test14 () :
+    i = 2
+    j = 3
+    i <<= j        # in-place bit shift left
+    assert i == 16
+    assert j ==  3
+
+def test15 () :
+    i = 10         # 1010
+    j = 12         # 1100
+    k = i & j      # 1000: bit and
+    assert i == 10
+    assert j == 12
+    assert k ==  8
+
+def test16 () :
+    i = 10
+    j = 12
+    i &= j         # in-place bit and
+    assert i ==  8
+    assert j == 12
+
+def test17 () :
+    i = 10         # 1010
+    j = 12         # 1100
+    k = i | j      # 1110: bit or
+    assert i == 10
+    assert j == 12
+    assert k == 14
+
+def test18 () :
+    i = 10
+    j = 12
+    i |= j         # in-place bit or
+    assert i == 14
+    assert j == 12
+
+def test19 () :
+    i = 10         # 1010
+    j = 12         # 1100
+    k = i ^ j      # 0110: bit exclusive or
+    assert i == 10
+    assert j == 12
+    assert k ==  6
+
+def test20 () :
+    i = 10
+    j = 12
+    i ^= j         # in-place bit exclusive or
+    assert i ==  6
+    assert j == 12
+
+def test21 () :
+    i = 10
+    j = 12
+    i ^= j
+    j ^= i
+    i ^= j
+    assert i == 12
+    assert j == 10
+
+def test22 () :
+    a = True
+    b = True
+    c = False
+    assert a and b
+    assert not (a and c)
+    assert a or b
+    assert a or c
+
+def test23 () :
+    s = "abc"
+    assert s[1]             == "b" # str index
+    assert s.__getitem__(1) == "b"
+    #s[1] = "d"                    # TypeError: 'str' object does not support item assignment
+
+def test24 () :
+    a = [2, 3, 4]
+    assert a[1]             == 3 # list index
+    assert a.__getitem__(1) == 3
+    a[1] = 5
+    assert a == [2, 5, 4]
+
+def test25 () :
+    u = (2, 3, 4)
+    assert u[1]             == 3 # tuple index
+    assert u.__getitem__(1) == 3
+    #u[1] = 5                    # TypeError: 'tuple' object does not support item assignment
+
+def test26 () :
+    a = [2, 3, 4]
+    assert a[ 1] == 3
+    assert a[-1] == 4
+
+    assert a[1 : 2] == [3] # list slicing
+
+    s = slice(1, 2)
+    assert s.start          == 1
+    assert s.stop           == 2
+    assert a[s]             == [3]
+    assert a.__getitem__(s) == [3]
+
+    assert a[ 1 : 3] == [3, 4]
+    assert a[-2 : 3] == [3, 4]
+    assert a[ 0 : 3] == [2, 3, 4]
+
+    assert a[0 : 3 : 2] == [2, 4]
+
+    s = slice(0, 3, 2)
+    assert s.start          == 0
+    assert s.stop           == 3
+    assert s.step           == 2
+    assert a[s]             == [2, 4]
+    assert a.__getitem__(s) == [2, 4]
+
+    assert a[2 : -4 : -2] == [4, 2]
+    assert a[:]           == [2, 3, 4]
+
+def test27 () :
+    a = [2, 3, 4]
+    b = a[:]
+    assert a is not b
+    assert a ==     b
+    b[1] += 1
+    assert a == [2, 3, 4]
+    assert b == [2, 4, 4]
+
+def test28 () :
+    u = (2, 3, 4)
+    v = u[:]
+    assert u is v
+
+def test29 () :
+    a = [2, 3, 4]
+    b = copy(a)
+    assert a is not b
+    assert a ==     b
+    b[1] += 1
+    assert a == [2, 3, 4]
+    assert b == [2, 4, 4]
+
+def test30 () :
+    u = (2, 3, 4)
+    v = copy(u)
+    assert u is v
+
+def test31 () :
+    a = [2, 3, 4]
+    b = [1, a, 5]
+    c = b[:]
+    assert b    is not c
+    assert b    ==     c
+    assert b[1] is     c[1]
+
+def test32 () :
+    a = [2, 3, 4]
+    b = [1, a, 5]
+    c = copy(b)
+    assert b    is not c
+    assert b    ==     c
+    assert b[1] is     c[1]
+
+def test33 () :
+    a = [2, 3, 4]
+    b = [1, a, 5]
+    c = deepcopy(b)
+    assert b    is not c
+    assert b    ==     c
+    assert b[1] is not c[1]
+    assert b[1] ==     c[1]
+
+def test34 () :
+    s = "a"
+    t = "bc"
+    u = s + t             # string concatenation
+    assert u is not "abc"
+    assert u ==     "abc"
+
+def test35 () :
+    a = [2]
+    b = [3, 4]
+    c = a + b                 # list concatenation
+    assert c is not [2, 3, 4]
+    assert c ==     [2, 3, 4]
+    assert c !=     (2, 3, 4)
+
+def test36 () :
+    u = (2,)
+    v = (3, 4)
+    w = (u + v)               # tuple concatenation
+    assert w is not (2, 3, 4)
+    assert w ==     (2, 3, 4)
+    assert w !=     [2, 3, 4]
+
+def test37 () :
+    s = "abc"
+    t = 2 * s                # string replication
+    assert t is not "abcabc"
+    assert t ==     "abcabc"
+
+def test38 () :
+    a = [2, 3, 4]
+    b = 2 * a                          # list replication
+    assert b is not [2, 3, 4, 2, 3, 4]
+    assert b ==     [2, 3, 4, 2, 3, 4]
+
+def test39 () :
+    u = (2, 3, 4)
+    v = 2 * u                          # tuple replication
+    assert u is not (2, 3, 4, 2, 3, 4)
+    assert v ==     (2, 3, 4, 2, 3, 4)
+
+def test40 () :
+    a = [2, 3, 4]
+    b = a
+    assert a is b
+    b += [5]
+    assert a == [2, 3, 4, 5]
+    assert a is b
+
+def test41 () :
+    a = [2, 3, 4]
+    b = a
+    assert a is b
+    b += (5,)
+    assert a == [2, 3, 4, 5]
+    assert a is b
+
+def test42 () :
+    a = [2, 3, 4]
+    b = a
+    assert a is b
+    b = b + [5]
+    assert a == [2, 3, 4]
+    assert b == [2, 3, 4, 5]
+
+def test43 () :
+    a = [2, 3, 4]
+    b = a
+    assert a is b
+    #b = b + (5,) # TypeError: can only concatenate list (not "tuple") to list
+
+def test44 () :
+    x = (2, 3, 4)
+    y = x
+    assert x is y
+    y += (5,)
+    assert x == (2, 3, 4)
+    assert y == (2, 3, 4, 5)
+
+def test45 () :
+    u = (2, 3, 4)
+    v = u
+    assert u is v
+    #v += [5]     # TypeError: can only concatenate tuple (not "list") to tuple
+
+def test46 () :
+    x = (2, 3, 4)
+    y = x
+    assert x is y
+    y = y + (5,)
+    assert x == (2, 3, 4)
+    assert y == (2, 3, 4, 5)
+
+def test47 () :
+    u = (2, 3, 4)
+    v = u
+    assert u is v
+    #v = v + [5]  # TypeError: can only concatenate tuple (not "list") to tuple
+
+def test48 () :
+    i = 3
+    j = 5
+    k = 7
+    l = 8
+    assert (i < j) and (j < k) and (k < l)
+    assert i < j < k < l
+
+def test49 () :
+    i = 10
+    j = 12
+    i, j = j, i
+    assert i == 12
+    assert j == 10
+
+def test50 () :
+    a = [2, 3]
+    i, j = a
+    assert i == 2
+    assert j == 3
+
+def test51 () :
+    class A :
+        k = 4
+
+        def __init__ (self, i, j) :
+            self.i = i
+            self.j = j
+
+    x = A(2, 3)
+    assert x.i == 2
+    assert x.j == 3
+    assert A.k == 4
+
+def main () :
+    print("Operators.py")
+    for i in range(51) :
+        eval("test" + str(i + 1) + "()")
+    print("Done.")
+
+if __name__ == "__main__" : # pragma: no cover
+    main()
