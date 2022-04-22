8196b2583140a95d5d2ced13110b7637b035a36e
Glenn Downing <downing@cs.utexas.edu>
2022-04-22 12:02:15 -0500

    another commit

--- notes/04-22.java
+++ notes/04-22.java
@@ -1,5 +1,5 @@
 // -----------
-// Thu, 22 Apr
+// Fri, 22 Apr
 // -----------
 
 /*
@@ -23,9 +23,400 @@ Takeaways:
 11. relational algebra is closed on all operations
 12. Python, C, Java are procedural; SQL is declarative
 13. some queries can't be done with a join, and need a subquery
+14. prefer enums to final ints
+15. prefer generic containers
+16. prefer StringBuilder to strings
 */
 
 /*
 StrategyPattern1.java
+StrategyPattern2.java
 */
 
+// ---------------------
+// StrategyPattern1.java
+// ---------------------
+
+/*
+Extract Method (110)
+create Customer.amountMethod()
+
+Replace Temp with Query (120)
+changed thisAmount to amountMethod()
+
+Move Method (142)
+move Customer.amountMethod() to Rental.amount()
+*/
+
+import java.util.Vector;
+
+class Movie {
+    enum PriceCode ...
+
+    public static final int REGULAR     = 0;
+    public static final int NEW_RELEASE = 1;
+    public static final int CHILDRENS   = 2;
+
+    private String _title;
+    private int    _priceCode;
+
+    public Movie (String title, PriceCode priceCode) {
+        _title = title;
+        setPriceCode(priceCode);}
+
+    public int getPriceCode () { // const
+        return _priceCode;}
+
+    public String getTitle () { // const
+        return _title;}
+
+    public void setPriceCode (int priceCode) {
+        _priceCode = priceCode;}}
+
+class Rental { // one-to-one relationship with movie
+    private Movie _movie;
+    private int   _daysRented;
+
+    public Rental (Movie movie, int daysRented) {
+        _movie      = movie;
+        _daysRented = daysRented;}
+
+    public int getDaysRented () { // const
+        return _daysRented;}
+
+    public Movie getMovie () { // const
+        return _movie;}
+
+    public double amount () {
+        double thisAmount = 0;
+        switch (getMovie().getPriceCode()) { // could change getMove() to _movie
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2)     // could change getDaysRented() to _daysRented
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3)
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                break;}
+        return thisAmount;}
+    }
+
+/*
+access directives in Java
+1. public,    everyone sees it
+2. private,   only the class sees it
+3. protected, descendents and package
+4. <nothing>, package
+*/
+
+class Customer { // one-to-many relationship with rental
+    private String         _name;
+    private Vector<Rental> _rentals = new Vector<Rental>();
+
+    public Customer (String name) {
+        _name = name;}
+
+    public void addRental (Rental rental) {
+        _rentals.addElement(rental);}
+
+    public String statement () {                                         // const, O(n)
+        double amount = 0;
+        int    points = 0;
+        String result = "Rental Record for " + _name + "\n";
+        for (Rental rental : _rentals) {
+            amount += rental.amount();
+            ++points;
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                (rental.getDaysRented()           >  1))
+                ++points;
+            result +=
+                "\t" + rental.getMovie().getTitle() +
+                "\t" + String.valueOf(rental.amount()) + "\n";}
+        result +=
+            "Amount owed is "      +
+            String.valueOf(amount) + "\n";
+        result +=
+            "You earned "              +
+            String.valueOf(points)     +
+            " frequent renter points";
+        return result;}}
+
+/*
+iterable in Python
+responds to __iter__ with an iterator, which responds to __next__, __iter__
+
+iterable in Java
+responds to iterator with an iterator, which responds to hasNext(), and next()
+*/
+
+ArrayList x = new ArrayList...
+Iterator  p = x.iterator();
+while (p.hasNext())
+    ...p.next()...
+
+final class StrategyPattern1 {
+    private static void my_assert(Boolean b) {
+        if (!b)
+            throw new RuntimeException();}
+
+    public static void main (String[] args) {
+        System.out.println("StrategyPattern1.java");
+
+        Customer x = new Customer("Penelope");
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "Amount owed is 0.0\n"         +
+            "You earned 0 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Shane", Movie.REGULAR), 2));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "Amount owed is 2.0\n"         +
+            "You earned 1 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Red River", Movie.REGULAR), 5));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "Amount owed is 8.5\n"         +
+            "You earned 2 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Giant", Movie.NEW_RELEASE), 1));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "Amount owed is 11.5\n"        +
+            "You earned 3 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("2001", Movie.NEW_RELEASE), 3));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "\t2001\t9.0\n"                +
+            "Amount owed is 20.5\n"        +
+            "You earned 5 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Big Country", Movie.CHILDRENS), 3));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "\t2001\t9.0\n"                +
+            "\tBig Country\t1.5\n"         +
+            "Amount owed is 22.0\n"        +
+            "You earned 6 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Spartacus", Movie.CHILDRENS), 5));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "\t2001\t9.0\n"                +
+            "\tBig Country\t1.5\n"         +
+            "\tSpartacus\t4.5\n"           +
+            "Amount owed is 26.5\n"        +
+            "You earned 7 frequent renter points"));
+
+        System.out.println("Done.");}}
+
+
+
+
+
+
+
+
+
+// ---------------------
+// StrategyPattern2.java
+// ---------------------
+
+/*
+Extract Method (110)
+Move Method (142)
+create Rental.points()
+
+Extract Method (110)
+Move Method (142)
+create Rental.statement()
+*/
+
+import java.util.Vector;
+
+class Movie {
+    public static final int REGULAR     = 0;
+    public static final int NEW_RELEASE = 1;
+    public static final int CHILDRENS   = 2;
+
+    private String _title;
+    private int    _priceCode;
+
+    public Movie (String title, int priceCode) {
+        _title = title;
+        setPriceCode(priceCode);}
+
+    public int getPriceCode () { // const
+        return _priceCode;}
+
+    public String getTitle () { // const
+        return _title;}
+
+    public void setPriceCode (int priceCode) {
+        _priceCode = priceCode;}}
+
+class Rental {
+    private Movie _movie;
+    private int   _daysRented;
+
+    public Rental (Movie movie, int daysRented) {
+        _movie      = movie;
+        _daysRented = daysRented;}
+
+    public int getDaysRented () { // const
+        return _daysRented;}
+
+    public Movie getMovie () { // const
+        return _movie;}
+
+    public double amount () {                              // const
+        double result = 0;
+        switch (getMovie().getPriceCode()) {               // why not _movie?
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)                   // why not _daysRented?
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;}
+        return result;}
+
+    public int points () {
+         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            (getDaysRented()           >  1))
+            return 2;
+        return 1;}
+
+    public string statment () { // we should use StringBuilder
+        return
+        "\t" + getMovie().getTitle()    +
+        "\t" + String.valueOf(rental.amount()) + "\n";}            // rental.amount() again!
+}
+
+class Customer {
+    private String         _name;
+    private Vector<Rental> _rentals = new Vector<Rental>();
+
+    public Customer (String name) {
+        _name = name;}
+
+    public void addRental (Rental rental) {
+        _rentals.addElement(rental);}
+
+    public String statement () {                                           // const, O(n)
+        double amount = 0;
+        int    points = 0;
+        String result = "Rental Record for " + _name + "\n";
+        for (Rental rental : _rentals) {
+            amount += rental.amount();
+            points += rental.points();
+            result += rental.statement();
+        result +=
+            "Amount owed is "      +
+            String.valueOf(amount) + "\n";
+        result +=
+            "You earned "              +
+            String.valueOf(points)     +
+            " frequent renter points";
+        return result;}}
+
+final class StrategyPattern2 {
+    private static void my_assert(Boolean b) {
+        if (!b)
+            throw new RuntimeException();}
+
+    public static void main (String[] args) {
+        System.out.println("StrategyPattern2.java");
+
+        Customer x = new Customer("Penelope");
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "Amount owed is 0.0\n"         +
+            "You earned 0 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Shane", Movie.REGULAR), 2));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "Amount owed is 2.0\n"         +
+            "You earned 1 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Red River", Movie.REGULAR), 5));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "Amount owed is 8.5\n"         +
+            "You earned 2 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Giant", Movie.NEW_RELEASE), 1));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "Amount owed is 11.5\n"        +
+            "You earned 3 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("2001", Movie.NEW_RELEASE), 3));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "\t2001\t9.0\n"                +
+            "Amount owed is 20.5\n"        +
+            "You earned 5 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Big Country", Movie.CHILDRENS), 3));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "\t2001\t9.0\n"                +
+            "\tBig Country\t1.5\n"         +
+            "Amount owed is 22.0\n"        +
+            "You earned 6 frequent renter points"));
+
+        x.addRental(new Rental(new Movie("Spartacus", Movie.CHILDRENS), 5));
+        my_assert(x.statement().equals(
+            "Rental Record for Penelope\n" +
+            "\tShane\t2.0\n"               +
+            "\tRed River\t6.5\n"           +
+            "\tGiant\t3.0\n"               +
+            "\t2001\t9.0\n"                +
+            "\tBig Country\t1.5\n"         +
+            "\tSpartacus\t4.5\n"           +
+            "Amount owed is 26.5\n"        +
+            "You earned 7 frequent renter points"));
+
+        System.out.println("Done.");}}
---
+++ notes/04-25.java
@@ -0,0 +1,29 @@
+// -----------
+// Mon, 25 Apr
+// -----------
+
+/*
+Takeaways:
+
+ 1. assertions are not good for testing
+ 2. assertions are not good for user errors
+ 3. frozenset, str, tuple are immutable in content and size
+ 4. set is immutable in content
+ 5. iter on an iterator returns itself
+ 6. iterators are exhaustible, containers are not
+ 7. generators are iterators
+ 8. generators and map and filter capture objects!!!
+ 9. lambdas capture names!!!
+10. avoid mutable defaults
+11. relational algebra is closed on all operations
+12. Python, C, Java are procedural; SQL is declarative
+13. some queries can't be done with a join, and need a subquery
+14. prefer enums to final ints
+15. prefer generic containers
+16. prefer StringBuilder to strings
+*/
+
+/*
+StrategyPattern3.java
+*/
+
